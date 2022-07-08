7b7d14411a688c5c3af67e4de9d3258240e3549a
Diether1234 <108930539+Diether1234@users.noreply.github.com>
2022-07-08 14:05:17 +0400

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+name = "john"
+if name =="john":
+    print("teacher is smith")
+elif name == "aaira":
+    print("Teacher is joy")
+elif name == "Diether":
+    print("teacher is leena")
+elif name == "Nitish":
+    print("teacher is Ash")
+else:
+    print("no kid in the camp with this name")
+
